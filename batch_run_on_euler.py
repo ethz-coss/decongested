5510f8c5e700eba385c672cd6331190f34745188
ccarissimo <N20kztu!>
2023-05-26 14:55:02 +0200

    update for euler cluster runs

--- batch_run_on_euler.py
+++ batch_run_on_euler.py
@@ -6,12 +6,4 @@ N_ITER = 400000
 for NEXT_DESTINATION_METHOD in ["random", "work-commute"]:
     for EXPLORATION_METHOD in ["random", "neighbours"]:
         for IOT_NODES in [True, False]:
-            os.system(f'sbatch '
-                      f'--mem-per-cpu=8G '
-                      f'--time=24:00:00 '
-                      f'--wrap="python dqn_grid_online.py '
-                      f'{N_ITER} '
-                      f'{NEXT_DESTINATION_METHOD} '
-                      f'{EXPLORATION_METHOD} '
-                      f'{IOT_NODES} '
-                      f'{SAVE_PATH}')
+            os.system(f'sbatch --mem-per-cpu=8G --time=24:00:00 --wrap="python dqn_grid_online.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {IOT_NODES} {SAVE_PATH}')
