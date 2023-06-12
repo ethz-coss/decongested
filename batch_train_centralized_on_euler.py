e2cf61759bc6c9dd93c06aef759b32a19d0ce0b0
ccarissimo <N20kztu!>
2023-06-12 15:10:40 +0200

    centralized agent training cluster

---
+++ batch_train_centralized_on_euler.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("module load gcc/8.2.0 python/3.9.9")  # load appropriate modules for EULER cluster
+
+SAVE_PATH = "/cluster/scratch/ccarissimo/decongested"
+N_ITER = 400000
+
+for GRID in ["uniform", "random", "braess"]:
+    for NEXT_DESTINATION_METHOD in ["simple", "one-way", "random", "work-commute"]:
+        if GRID == "braess" and NEXT_DESTINATION_METHOD != "one-way":
+            continue
+        for EXPLORATION_METHOD in ["random", "neighbours"]:
+            for IOT_NODES in [True, False]:
+                os.system(f'sbatch sbatch --mem-per-cpu=32G --gpus=1 --gres=gpumem:10g --time=48:00:00 --wrap="python dqn_train_centralized.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {SAVE_PATH} {GRID} {"--iot_nodes" if IOT_NODES else ""}"')
