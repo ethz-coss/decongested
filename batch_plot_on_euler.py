31dadc9b59fb18848a84158151eb61158855c114
ccarissimo <N20kztu!>
2023-06-12 14:02:33 +0200

    batch plots on cluster

--- batch_plot_on_euler.py
+++ batch_plot_on_euler.py
@@ -9,4 +9,4 @@ for GRID in ["uniform", "random", "braess"]:
             continue
         for EXPLORATION_METHOD in ["random", "neighbours"]:
             for IOT_NODES in [True, False]:
-                os.system(f'sbatch --mem-per-cpu=64G --time=48:00:00 --wrap="python dqn_grid_online.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {SAVE_PATH} {GRID} {"--iot_nodes" if IOT_NODES else ""}"')
+                os.system(f'sbatch --mem-per-cpu=32G --time=48:00:00 --wrap="python plotting.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {SAVE_PATH} {GRID} {"--iot_nodes" if IOT_NODES else ""}"')
