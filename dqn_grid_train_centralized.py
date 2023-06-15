90430770a389ec4927bebb41004bf788dddbc970
ccarissimo <N20kztu!>
2023-06-15 15:44:13 +0200

    bugfix tensors

--- dqn_grid_train_centralized.py
+++ dqn_grid_train_centralized.py
@@ -67,8 +67,9 @@ def train_centralized_agent_off_policy(n_iter, next_destination_method="simple",
 
             if use_agent_ids:
                 id = np.random.choice(possible_ids)
-                t["state"] = torch.cat((torch.tensor([[id]]), t["state"]), 1)
-                t["next_state"] = torch.cat((torch.tensor([[id]]), t["state"]), 1)
+                id_tensor = torch.tensor([[id]], dtype=torch.float32, device=DEVICE)
+                t["state"] = torch.cat((id_tensor, t["state"]), 1)
+                t["next_state"] = torch.cat((id_tensor, t["state"]), 1)
             agent.memory.push(
                 t["state"].to(DEVICE),
                 t["action"].to(DEVICE),
