5f21184c95810c81faad01aa7b1ce123c0dfa14f
ccarissimo <N20kztu!>
2023-06-16 10:24:36 +0200

    batch run centralized with ids

--- batch_train_centralized_on_euler.py
+++ batch_train_centralized_on_euler.py
@@ -3,16 +3,14 @@ import os
 os.system("module load gcc/8.2.0 python/3.9.9")  # load appropriate modules for EULER cluster
 
 SAVE_PATH = "/cluster/scratch/ccarissimo/decongested"
-N_ITER = 100
+N_ITER = 400000
 AGENT_IDS = True
 
-# for GRID in ["uniform", "random", "braess"]:
-#     for NEXT_DESTINATION_METHOD in ["simple", "one-way", "random", "work-commute"]:
-#         if GRID == "braess" and NEXT_DESTINATION_METHOD != "one-way":
-#             continue
-#         for EXPLORATION_METHOD in ["random", "neighbours"]:
-#             for IOT_NODES in [True, False]:
-#                 os.system(f'sbatch --mem-per-cpu=64G --gpus=1 --gres=gpumem:32g --time=48:00:00 --wrap="python dqn_grid_train_centralized.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {SAVE_PATH} {GRID} {"--iot_nodes" if IOT_NODES else ""} {"--with_agent_ids" if AGENT_IDS else ""}"')
+for GRID in ["uniform", "random"]:
+    for NEXT_DESTINATION_METHOD in ["simple", "one-way", "random", "work-commute"]:
+        for EXPLORATION_METHOD in ["random", "neighbours"]:
+            for IOT_NODES in [True, False]:
+                os.system(f'sbatch --mem-per-cpu=64G --gpus=1 --gres=gpumem:32g --time=48:00:00 --wrap="python dqn_grid_train_centralized.py {N_ITER} {NEXT_DESTINATION_METHOD} {EXPLORATION_METHOD} {SAVE_PATH} {GRID} {"--iot_nodes" if IOT_NODES else ""} {"--with_agent_ids" if AGENT_IDS else ""}"')
 
 # test
-os.system(f'sbatch --mem-per-cpu=64G --gpus=1 --gres=gpumem:32g --time=48:00:00 --wrap="python dqn_grid_train_centralized.py 1000 simple random {SAVE_PATH} uniform --iot_nodes --with_agent_ids"')
+# os.system(f'sbatch --mem-per-cpu=64G --gpus=1 --gres=gpumem:32g --time=48:00:00 --wrap="python dqn_grid_train_centralized.py 1000 simple random {SAVE_PATH} uniform --iot_nodes --with_agent_ids"')
