dd968110d3fe248dc739d107a36d162e9a39ee23
CCarissimo <cesarecarissimo@gmail.com>
2023-06-19 12:15:33 +0200

    batch, only nonstationary

--- batch_evaluate_on_euler.py
+++ batch_evaluate_on_euler.py
@@ -6,27 +6,27 @@ os.system("module load gcc/8.2.0 python/3.9.9")  # load appropriate modules for 
 SAVE_PATH = "/cluster/scratch/ccarissimo/decongested"
 N_ITER = 400000
 
-for GRID in ["uniform", "random"]:
-    for NEXT_DESTINATION_METHOD in ["one-way", "work-commute"]:
-        for EXPLORATION_METHOD in ["random"]:
-            for IOT_NODES in [True]:
-                for RATIO in np.linspace(0, 1, 21):
-                    for AGENT_IDS in [True, False]:
-                        os.system(f'sbatch '
-                                  f'--mem-per-cpu=16G '
-                                  f'--gpus=1 '
-                                  f'--time=12:00:00 '
-                                  f'--job-name=evaluate_{GRID}_{NEXT_DESTINATION_METHOD}_{RATIO}_{AGENT_IDS} '
-                                  f'--wrap="python dqn_grid_evaluate_online.py '
-                                  f'{N_ITER} '
-                                  f'{NEXT_DESTINATION_METHOD} '
-                                  f'{EXPLORATION_METHOD} '
-                                  f'{SAVE_PATH} '
-                                  f'{GRID} '
-                                  f'{RATIO} '
-                                  f'--iot_nodes '
-                                  f'{"--with_ids" if AGENT_IDS else ""}"'
-                                  )
+# for GRID in ["uniform", "random"]:
+#     for NEXT_DESTINATION_METHOD in ["one-way", "work-commute"]:
+#         for EXPLORATION_METHOD in ["random"]:
+#             for IOT_NODES in [True]:
+#                 for RATIO in np.linspace(0, 1, 21):
+#                     for AGENT_IDS in [True, False]:
+#                         os.system(f'sbatch '
+#                                   f'--mem-per-cpu=16G '
+#                                   f'--gpus=1 '
+#                                   f'--time=12:00:00 '
+#                                   f'--job-name=evaluate_{GRID}_{NEXT_DESTINATION_METHOD}_{RATIO}_{AGENT_IDS} '
+#                                   f'--wrap="python dqn_grid_evaluate_online.py '
+#                                   f'{N_ITER} '
+#                                   f'{NEXT_DESTINATION_METHOD} '
+#                                   f'{EXPLORATION_METHOD} '
+#                                   f'{SAVE_PATH} '
+#                                   f'{GRID} '
+#                                   f'{RATIO} '
+#                                   f'--iot_nodes '
+#                                   f'{"--with_ids" if AGENT_IDS else ""}"'
+#                                   )
 
 AGENT_IDS = True
 for GRID in ["uniform"]:
